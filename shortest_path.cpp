87a9c2585b1a2585520b5fdb39ee35320c3587c4
Zack Fielding <zackfielding@gmail.com>
2023-02-09 21:34:51 -0800

    dsf

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <unordered_set>
 
 // weighted_graph class will call on vertex ctors
 class weighted_graph;
@@ -11,6 +12,8 @@ class vertex {
     vertex(const std::string str) : name {str}, adjacent_weight_map {} {}
     friend class ::weighted_graph;
 };
+
+enum class to_print : int {NO_PRINT = 0, PRINT = 1};
 
 class weighted_graph {
     std::unordered_map<std::string, vertex*> map;
@@ -24,19 +27,22 @@ public:
     vertex* add_vertex(const std::string);
     void add_adjacent(vertex*, const std::string, const int weight);
     // TO DO: implement DFS (? search for a vertex by string ? option to read out all nodes as the algo executes)
-    //vertex* depth_first_search(const std::string to_find);
+    void depth_first_search(vertex* vert, const to_print, std::unordered_set<const vertex*>*);
+     // return an access point for search algos
+    vertex* getVertexAccess(const std::string str) { return map.find(str)->second; }
 };
 
 vertex* weighted_graph::add_vertex(const std::string str){
-     // create new vertex on heap && add to map
-    vertex *n_vert = new vertex{str};
-    auto check_insert {map.insert({str, n_vert})};
-     // check to see if insertion works, i.e., all vertices must have unique names
-    if (!check_insert.second){
-        std::clog << "[ERROR]: failed to create new vertex (likely duplicate name).\n";
-        delete n_vert;
-        return nullptr;
-    }
+    // check to see if vertex has already been created
+    auto check_if_exist_iter {map.find(str)};
+
+    // if it doesn't exist => create new one, ELSE => assign ptr
+    vertex* n_vert {nullptr};
+    if (check_if_exist_iter == map.end()){
+        n_vert = new vertex{str};
+        map.insert({str, n_vert});
+    } else n_vert = check_if_exist_iter->second;
+
     return n_vert;
 }
 
@@ -72,11 +78,38 @@ void createWeightedGraph(std::ifstream& file, weighted_graph *wgraph){
     return;
 }
 
+void weighted_graph::depth_first_search(vertex* vert, const to_print e_print, std::unordered_set<const vertex*> *track){
+    std::unordered_set<const vertex*>::iterator iter {track->find(vert)};
+     // base case
+    if (iter != track->end()){
+        return;
+    }
+
+     // handle print enum
+    switch (e_print) {
+        case (to_print::PRINT): {
+           std::cout << vert->name << ' '; 
+           break;
+        }
+        case (to_print::NO_PRINT) : {
+            break;
+        }
+    }
+
+    track->insert(vert);
+    for (std::pair<vertex*, int> p : vert->adjacent_weight_map){
+        this->depth_first_search(p.first, e_print, track);
+    }
+    return;
+}
+
 int main(){
     std::ifstream file {"shortest_path_data.txt"};
     if (file){
         weighted_graph wgraph {};
         createWeightedGraph(file, &wgraph);
+        std::unordered_set<const vertex*> track;
+        wgraph.depth_first_search(wgraph.getVertexAccess("ElPaso"), to_print::PRINT, &track);
         file.close();
     }
 
